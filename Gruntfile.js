5eb82a830e9b057f312fbe465303a28a8982aeb6
Tyler Kellen <tyler@sleekcode.net>
2012-11-27 15:18:00 -0600

    fix npm test

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,14 +4,14 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*_test.js'],
-    },
     jshint: {
       options: {
         jshintrc: '.jshintrc'
       },
       all: ['Gruntfile.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js'],
     }
   });
 
